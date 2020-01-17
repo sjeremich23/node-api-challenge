0c0713be8dd835bcd92751037848f74e3b008d8d
Shane Jeremich <shane.jeremich@gmail.com>
2020-01-17 10:08:16 -0700

    set up server.js, set up folder structure for api folder

---
+++ api/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const helmet = require("helmet");
+const projectsRouter = require("../api/routers/projects/projectsRouter");
+const actionsRouter = require("../api/routers/actions/actionsRouter");
+
+const server = express();
+server.use(express.json());
+server.use(logger);
+server.use(helmet());
+server.use("/api/projects", projectsRouter);
+server.use("/api/actions", actionsRouter);
+
+server.get("/", (req, res) => {
+  res.send({
+    Server: "Hi, I'm your Server.  Can I start you off with a drink?"
+  });
+});
+
+function logger(req, res, next) {
+  const { method, url } = req;
+  console.log(
+    `[${new Date().toLocaleTimeString()}] ${method} request to URL: ${url}`
+  );
+  next();
+}
+
+module.exports = server;
